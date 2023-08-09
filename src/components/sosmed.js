47d09940014f3ce11735b07a780ce3b97a4a3b29
ACER <apipska.10@gmail.com>
2023-08-09 21:49:52 +0700

    Added link

--- src/components/sosmed.js
+++ src/components/sosmed.js
@@ -2,7 +2,7 @@ const Sosmed = () => {
     return (
         <>
             <div className="sosmed-wrapper">
-                <a className="sosmed-item">
+                <a href="https://github.com/pipsyclone" target="_blank" className="sosmed-item">
                     <i className="fa-brands fa-github"></i>
                 </a>
                 <a className="sosmed-item">
@@ -15,7 +15,7 @@ const Sosmed = () => {
             </div>
 
             <div className="email-wrapper">
-                <a className="email-item">
+                <a href="mailto:apiprs24@gmail.com" className="email-item">
                     apiprs24@gmail.com
                 </a>
                 <div className="vertical-rule"></div>
