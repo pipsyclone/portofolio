d17628ed1491afd5b5981de75b495ddba76c4e36
ACER <apipska.10@gmail.com>
2023-11-01 19:15:10 +0700

    Responsible Layout

--- src/data/project.js
+++ src/data/project.js
@@ -1,94 +1,48 @@
 const DataProject = [
     {
-        no: 1,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien.",
+        no: 3,
+        name: "Oneeku",
+        desc: "Oneeku adalah website untuk streaming anime yang telah saya buat tanpa database, ini adalah hasil scrapper data dari website lain.",
         tech: [
             {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
+            {name: "NodeJS"}
         ],
         github: true,
         linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
+        linkWebsite: "https://oneeku.vercel.app"
     },
     {
         no: 2,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien. klasdfjasl d dakfjadfd  ajkldfakdfh ajkdfhaj khdfjkahdj kfa jkdfhaa dfakdf",
+        name: "Wedding Invitation Digital",
+        desc: "Fungsi dari website ini adalah sebagai undangan digital untuk mempermudah mengundang tamu pernikahan.",
         tech: [
-            {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
+            {name: "ReactJs"}
         ],
         github: true,
         linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
+        linkWebsite: "https://weddinginvitationdigital.vercel.app"
     },
     {
-        no: 3,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien.",
+        no: 1,
+        name: "Ied Card Digital",
+        desc: "Website ini dibuat untuk membuat sebuah poster dengan nama custom, yang nantinya dapat di download menjadi file gambar.",
         tech: [
-            {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
+            {name: "ReactJs"}
         ],
         github: true,
         linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
+        linkWebsite: "https://iedcarddigital.vercel.app"
     },
     {
         no: 4,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien.",
+        name: "MyQRCode",
+        desc: "Website ini dapat berfungsi untuk membuat qrcode dan juga dapat membaca qrcode, akan dikembangkan dimasa yang akan datang.",
         tech: [
-            {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
+            {name: "ReactJs"}
         ],
         github: true,
         linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
-    },
-    {
-        no: 5,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien.",
-        tech: [
-            {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
-        ],
-        github: true,
-        linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
-    },
-    {
-        no: 6,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien.",
-        tech: [
-            {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
-        ],
-        github: true,
-        linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
-    },
-    {
-        no: 7,
-        name: "Sistem Informasi Perpustakaan",
-        desc: "Sistem ini dapat membantu manajemen perpustakaan yang tidak efisien.",
-        tech: [
-            {name: "ReactJs"},
-            {name: "NodeJS"},
-            {name: "MySQL"}
-        ],
-        github: true,
-        linkGithub: "https://github.com",
-        linkWebsite: "https://sispus.vercel.app"
+        linkWebsite: "https://reactjsmyqrcode.vercel.app"
     },
 ]
 
--- src/sections/project.js
+++ src/sections/project.js
@@ -6,50 +6,59 @@ const Project = () => {
         slice, maxItemProject, handleLoadMore
     } = Script()
 
+    console.log(slice.length)
+
     return (
         <>
             <div className="project-wrapper">
                 {
-                    slice.map((data, key) => {
-                        return (
-                            <a href={data.linkWebsite} target="_blank" className="project-card">
-                                <div className="project-icon">
-                                    <div>
-                                        <i className="fa-regular fa-folder project-icon-folder"></i>
+                    slice.length < 1 ?
+                        <>
+                            <p className="project-zero"></p>
+                            <p className="project-zero">Belum ada projek yang tersedia!_</p>
+                            <p className="project-zero"></p>
+                        </>
+                    :
+                        slice.map((data, key) => {
+                            return (
+                                <a href={data.linkWebsite} target="_blank" className="project-card">
+                                    <div className="project-icon">
+                                        <div>
+                                            <i className="fa-regular fa-folder project-icon-folder"></i>
+                                        </div>
+                                        <div className="ms-auto project-icon-others">
+                                            {
+                                                data.github ? 
+                                                    <a href={data.linkGithub} target="_blank" className="project-icon-github me-3">
+                                                        <i className="fa-brands fa-github"></i>
+                                                    </a>
+                                                :
+                                                    ""
+                                            }
+                                            <a href={data.linkWebsite} target="_blank" className="project-icon-link">
+                                                <i className="fa-solid fa-link"></i>
+                                            </a>
+                                        </div>
                                     </div>
-                                    <div className="ms-auto project-icon-others">
+
+                                    <div className="project-title">
+                                        {data.name}
+                                    </div>
+
+                                    {data.desc}
+
+                                    <div className="project-technologies">
                                         {
-                                            data.github ? 
-                                                <a href={data.linkGithub} target="_blank" className="project-icon-github me-3">
-                                                    <i className="fa-brands fa-github"></i>
-                                                </a>
-                                            :
-                                                ""
+                                            data.tech.map((technologies, key) => {
+                                                return (
+                                                    <> {technologies.name} </>
+                                                )
+                                            })
                                         }
-                                        <a href={data.linkWebsite} target="_blank" className="project-icon-link">
-                                            <i className="fa-solid fa-link"></i>
-                                        </a>
                                     </div>
-                                </div>
-
-                                <div className="project-title">
-                                    {data.name}
-                                </div>
-
-                                {data.desc}
-
-                                <div className="project-technologies">
-                                    {
-                                        data.tech.map((technologies, key) => {
-                                            return (
-                                                <> {technologies.name} </>
-                                            )
-                                        })
-                                    }
-                                </div>
-                            </a>
-                        )
-                    })
+                                </a>
+                            )
+                        })
                 }
             </div>
 
